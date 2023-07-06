7ff8101fffe1c6c97437707cde42f2b723b5a11b
tuanvu0412 <tuanvu041299@gmail.com>
2023-07-06 10:48:33 +0700

    tuanvu update lần 5

--- project_book/src/main/java/com/example/project_book/model/User.java
+++ project_book/src/main/java/com/example/project_book/model/User.java
@@ -24,7 +24,7 @@ public class User {
     @JsonIgnore
     private String passUser;
     @ManyToOne
-    @JoinColumn(name="nameRole",nullable = false)
+    @JoinColumn(name="idRole",nullable = false)
     private RoleUser roleUser;
 
     private boolean isDelete =false;
--- project_book/src/main/java/com/example/project_book/repository/home/IHomeRepesitory.java
+++ project_book/src/main/java/com/example/project_book/repository/home/IHomeRepesitory.java
@@ -1,12 +1,12 @@
-package com.example.project_book.repository.home;
-
-import com.example.project_book.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface IHomeRepesitory extends JpaRepository<Product,Integer> {
-    List<Product> findByisDeleteIsFalse();
-
-    List<Product> findByisDeleteIsFalseAndTypeProduct_idTypeProductIs(int idType);
-}
+//package com.example.project_book.repository.home;
+//
+//import com.example.project_book.model.Product;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface IHomeRepesitory extends JpaRepository<Product,Integer> {
+//    List<Product> findByisDeleteIsFalse();
+//
+//    List<Product> findByisDeleteIsFalseAndTypeProduct_idTypeProductIs(int idType);
+//}
