010867d92f57ea6991c0e45c638e975d39a3ef68
nguyenanhnhan95 <nguyenanhnhan95@gmail.com>
2023-07-10 20:58:16 +0700

    update data 9 nhan

--- project_book/src/main/java/com/example/project_book/controller/HomeController.java
+++ project_book/src/main/java/com/example/project_book/controller/HomeController.java
@@ -128,9 +128,6 @@ public class HomeController {
         model.addAttribute("list", homeService.getlistBook());
         if (request.getUserPrincipal() == null) {
             model.addAttribute("check", "check");
-            String nameBook = String.valueOf(homeService.getProductBestSellByName().getNameProduct());
-            Product product = homeService.findBynameProductIs(nameBook);
-            model.addAttribute("bestSell",product);
         } else {
             String email = request.getUserPrincipal().getName();
             model.addAttribute("user", usersService.findByEmailUser(email));
